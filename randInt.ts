cbde5ccdfb8194b22f2f1ada4d4af16f3378a36a
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-04-19 21:19:18 +0200

    zmiana, w odpowiedzi na narzekania strict checkera w Angularz-e

--- randInt.ts
+++ randInt.ts
@@ -6,16 +6,28 @@
  * @returns {number} random value from a given range
  */
 function randInt(min: number, max?: number): number {
-    if (min < 0 || max < 0) {
-        throw new Error("range must include non negative numbers")
+
+    let result: number = 0;
+
+    for (let i = 0; i < 2; i++) {
+        if (arguments[i] < 0) {
+            throw new Error("range must not include negative numbers");
+        }
     }
     if (arguments.length === 1) {
         max = min;
         min = 0;
     }
+    // floats protection
     min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
+    // should always be true,
+    // but the stricter angular checker requires me to do that way
+    if (max) {
+        max = Math.floor(max);
+        result = Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    return result;
 }
 
 export default randInt;

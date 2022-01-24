88e3d5c1de1ccaefef1a332db645efe42af5faee
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-01-24 22:23:48 +0100

    kosmetyka: getPreFilledArr1d -> getArr1dPrefilledWithNum

--- arr1dPreFilled.ts
+++ arr1dPreFilled.ts
@@ -1,4 +1,4 @@
-function getPreFilledArr1d(len: number, fill: number): Array<number> {
+function getArr1dPrefilledWithNum(len: number, fill: number): Array<number> {
     let result = new Array(len);
     for (let i = 0; i < len; i++) {
         result[i] = fill;
@@ -6,4 +6,4 @@ function getPreFilledArr1d(len: number, fill: number): Array<number> {
     return result;
 }
 
-export default getPreFilledArr1d;
+export default getArr1dPrefilledWithNum;
